90fc1235de36f51ccb50e513dd4accbbe4f5dfbb
Pawel Macyszyn <macyszyn78@gmail.com>
2022-09-18 21:40:24 +0200

    Work with selenum

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 # project in Selenium
 
+import msvcrt
 from selenium import webdriver
 from time import sleep
+url_insta = 'https://www.instagram.com/'
+url_insta_log = 'https://www.instagram.com/accounts/login/?'
+url_google = 'https://www.google.com/'
+
+
 # from selenium.webdriver.support import expected_conditions
 # from selenium.webdriver.common.by import By
 
-# browser = webdriver.Chrome('./chromedriver.exe')
 
-chrome_options = webdriver.ChromeOptions('/chromedriver.exe', chrome_options = chrome_options)
+chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--incognito")
 
-browser = webdriver.Chrome()
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.get(url_insta_log)
 
-
-url_insta = 'https://www.instagram.com/'
-url_google = 'https://www.google.com/'
-
-browser.get(url_google)
-sleep(2)
-
-browser.close()
+# if msvcrt.getch()[0] == 27:
+#     print('End')
+sleep(5)
